be1922c020211634bcc4fa40279fc14f5a7f0e26
Rangerboy574 <jdeweese574@icloud.com>
2025-04-11 13:19:48 -0500

    Initial Commit

---
+++ TaskFlow Onboarding/TaskFlow_OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFlow_OnboardingApp.swift
+//  TaskFlow Onboarding
+//
+//  Created by Joseph DeWeese on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskFlow_OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskFlow OnboardingTests/TaskFlow_OnboardingTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFlow_OnboardingTests.swift
+//  TaskFlow OnboardingTests
+//
+//  Created by Joseph DeWeese on 4/11/25.
+//
+
+import Testing
+@testable import TaskFlow_Onboarding
+
+struct TaskFlow_OnboardingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
